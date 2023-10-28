37824daae5645cea848d53da33ad6a209de4c5f9
jeremysr <jeremysr@protonmail.com>
2023-10-28 14:40:16 -0600

    added logic to return all content from  indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,9 @@
 import { openDB } from 'idb';
+const jateDB = await openDB('jate', 1);
+const txW = jateDB.transaction('jate', 'readwrite');
+const storeW = txW.objectStore('jate');
+const txR = jateDB.transaction('jate', 'readwrite');
+const storeR = txR.objectStore('jate');
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -14,15 +19,16 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDB = await openDB('jate', 1);
-  const tx = jateDB.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.add({content:content});
+  const request = storeW.add({content:content});
   const result = await request;
   console.log('text editor data stored to indexDB', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const request = storeR.getAll();
+  const result = await request;
+  console.log('all stored db data:', result);
+};
 
 initdb();
